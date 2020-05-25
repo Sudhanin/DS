9ae57f4301695dfa262bc48604479318f428a5ed
sudhanin <sudharssanramessh@gmail.com>
2020-05-25 11:09:07 +0530

    BST

--- BST/Insert.c
+++ BST/Insert.c
@@ -1,12 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX_QUEUE 500
 struct tree
 {
     int d;
-    struct tree *left;
-    struct tree *right;
+    struct tree* left;
+    struct tree* right;
 
 };
+struct tree** create(int *front,int *rear){
+    struct tree **queue=(struct tree **) malloc(sizeof(struct tree *)*MAX_QUEUE);
+    *front=*rear=0;
+    return queue;
+}
+void enqueue(struct tree *temp,struct tree **queue,int *rear){
+    queue[*rear]=temp;
+    (*rear)++;
+    
+}
+struct tree* dequeue(struct tree **queue,int *front){
+    (*front)++;
+    return queue[*front-1];
+
+}
 struct tree *root;
 void insert(int b){
      struct tree *ptr,*parptr,*nodeptr;
@@ -72,13 +88,26 @@ void postorder(struct tree* root ){
     printf("%d->",root->d);
 
 }
-  
+void levelorder(struct tree* root){
+    int rear,front;
+    struct tree **queue=create(&front,&rear);
+    struct tree *temp=root;
+    while(temp!=NULL){
+        printf("%d->",temp->d);
+        if(temp->left)
+          enqueue(temp->left,queue,&rear);
+        if(temp->right)
+           enqueue(temp->right,queue,&rear);
+        temp=dequeue(queue,&front);
+    }
+    
+}
 int main(){
     int i,n,a;
-    printf("DFS of a tree\n 1.Preorder <root><left><right>\n 2.Inorder <left><root><right>\n 3.Postorder <left><right><root>\n");
+    printf("DFS of a tree\n 1.Preorder <root><left><right>\n 2.Inorder <left><root><right>\n 3.Postorder <left><right><root>\n4.LevelOrder ");
     printf("Enter your choice:");
     scanf("%d",&a);
-    do{
+    while(a==0|| a==9){
       int b;
       printf("\nEnter the element:");
       scanf("%d",&b);
@@ -89,8 +118,11 @@ int main(){
       inorder(root);
       printf("\nPostorder:");
       postorder(root);
+      printf("\nLevelorder:");
+      if  (a==9)
+      levelorder(root);
       printf("\nEnter the choice:");
       scanf("%d",&a);
-    }while(a==0);
+    }
     return 0;
 }
